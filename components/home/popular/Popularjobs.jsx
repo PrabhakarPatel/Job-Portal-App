69b9350bcc32ef859d56043ed8e2730a8100d4fd
PrabhakarPatel <prabhakarpatle@gmail.com>
2023-06-24 20:17:16 +0530

    JOB PORTAL APP

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+
+import styles from "./popularjobs.style";
+import { TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
+const Popularjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+  const [selectedJob, setSelectedJob] = useState();
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text> Something went Wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
